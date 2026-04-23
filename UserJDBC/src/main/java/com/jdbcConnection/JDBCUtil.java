80347af0e30c4460798763fa7cc05d603d5913e5
Ganesh <ganeshgarje716@gmail.com>
2026-04-24 00:31:50 +0530

    create connection

---
+++ UserJDBC/src/main/java/com/jdbcConnection/JDBCUtil.java
@@ -0,0 +1,38 @@
+package com.jdbcConnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBCUtil {
+
+	
+	private static final String url="jdbc:mysql://localhost:3306/";
+	
+	private static final String username="root";
+	
+	private static final String password="garje@716Garje";
+	
+	
+	
+	public static Connection createConnection() {
+		
+		Connection con = null;
+		try {
+			
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			
+			 con = DriverManager.getConnection(url, username, password);
+		}
+		catch (ClassNotFoundException | SQLException e ) {
+			
+			e.printStackTrace();
+		}
+		
+		return con;
+		
+	}
+	
+	
+	
+}
